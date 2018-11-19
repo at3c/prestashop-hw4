2c7659f596b74b992e315099d416ba7b2bc00ecc
at3c <bogdan_s86@ukr.net>
2018-11-19 23:00:17 +0200

    Initial commit

---
+++ src/test/java/CreateProduct.java
@@ -0,0 +1,29 @@
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+/**
+ * Created by Admin on 19.11.2018.
+ */
+public class CreateProduct {
+    @BeforeClass
+    public void setUp() {
+
+    }
+
+    @AfterClass
+    public void tearDown() {
+
+    }
+
+    @BeforeMethod
+    public void prepareEnvironment() {
+
+    }
+
+    @AfterMethod
+    public void clearEnvironment() {
+
+    }
+}
